af728cab808adfa3985bffcf7fe3eb2bc560ed1e
Goldorteps <agolfofree@hotmail.com>
2023-08-17 12:35:49 +0200

    correción imagen

--- src/js/component/HeroSecction/HeroSecction.js
+++ src/js/component/HeroSecction/HeroSecction.js
@@ -8,10 +8,6 @@ const HeroSection = () => {
     <section className={styles.hero}>
       <div className={styles['hero-content']}>
         <h1>¡Bienvenidos a nuestra aplicación de gestión de tickets, impulsada por ChatGPT!</h1>
-        <p>
-        Nuestra aplicación está diseñada para ofrecerte una solución eficiente y efectiva para manejar tus tickets de soporte y atención al cliente. Con la ayuda de ChatGPT, una inteligencia artificial de procesamiento del lenguaje natural, la gestión de tickets nunca ha sido tan fácil y rápida.
-        </p>
-        <button className={styles['start-button']}>Empezar</button>
       </div>
       <div className={styles['hero-image']}>
       <img src={empresa4} alt="Imagen de bienvenida" />
