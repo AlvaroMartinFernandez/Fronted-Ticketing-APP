622fb32a70bbe1475f4c18a801f0bc2cad8a4b27
Goldorteps <agolfofree@hotmail.com>
2023-08-21 13:15:09 +0200

    formulario con alert

--- src/js/component/contacto/contacto.js
+++ src/js/component/contacto/contacto.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './contacto.css'; 
+import Swal from 'sweetalert2'; 
 
 const Contacto = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Simulamos un envío exitoso
+    // Aquí podrías realizar llamadas a una API o servicio de backend en lugar de este timeout
+    setTimeout(() => {
+      Swal.fire({
+        icon: 'success',
+        title: 'Éxito',
+        text: 'El mensaje ha sido enviado con éxito',
+      });
+    }, 1000); // Simulamos el tiempo de envío
+
+    console.log('Formulario enviado');
+  };
+
   return (
     <div className="container">
-      <form action="action_page.php">
+      <form onSubmit={handleSubmit}>
         <label htmlFor="fname">First Name</label>
         <input type="text" id="fname" name="firstname" placeholder="Your name.." />
 
+       
         <label htmlFor="lname">Last Name</label>
         <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
 
         <label htmlFor="country">Country</label>
         <select id="country" name="country">
-          <option value="australia">Australia</option>
-          <option value="canada">Canada</option>
+          <option value="australia">España</option>
+          <option value="canada">Protugal</option>
           <option value="usa">USA</option>
         </select>
 
@@ -24,12 +42,11 @@ const Contacto = () => {
         <input type="submit" value="Submit" />
       </form>
     </div>
-
-    
   );
 };
 
 export default Contacto;
+
 
 
  
